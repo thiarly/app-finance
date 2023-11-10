28bf53353f8651561ea5e64cb6b5c8b3ed53f6f4
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-10 09:23:49 -0300

    Tipos de registro

--- AppFinance/src/pages/New/index.js
+++ AppFinance/src/pages/New/index.js
@@ -4,7 +4,7 @@ import { Background, Input, SubmitButton, SubmitText } from './styles';
 import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Header from '../../components/Header';
-
+import RegisterTypes from '../../components/ResgisterTypes';
 
 export default function New() {
     const [labelInput, setLabelInput] = useState('');
@@ -29,10 +29,12 @@ export default function New() {
                         value={valueInput}
                         onChangeText={(text) => setValueInput(text)}
                     />
+                    <RegisterTypes type={type} sendTypeChanged={(item) => setType(item)}/>    
 
                     <SubmitButton>
                         <SubmitText>Registrar</SubmitText>
                     </SubmitButton>
+
                 </SafeAreaView>
             </Background>
         </TouchableWithoutFeedback>
